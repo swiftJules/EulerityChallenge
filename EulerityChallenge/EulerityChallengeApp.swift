8fddc7cf0dad242fbff3f2e6721373d00c071677
Rave Bizz <ravebizz@Raves-MacBook-Air-4.local>
2022-03-22 15:54:52 -0400

    initial commit

---
+++ EulerityChallenge/EulerityChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerityChallengeApp.swift
+//  EulerityChallenge
+//
+//  Created by Rave Bizz on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct EulerityChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
