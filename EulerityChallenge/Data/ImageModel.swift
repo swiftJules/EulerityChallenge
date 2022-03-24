98f34bbeb1ed490e907e43d4916cd316013cf1b5
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-24 07:00:59 -0400

    add coding keys and initializer to add uuid to model

--- EulerityChallenge/Data/ImageModel.swift
+++ EulerityChallenge/Data/ImageModel.swift
@@ -8,6 +8,15 @@
 import Foundation
 
 struct ImageModel: Codable, Identifiable {
-    var id: Int?
+    var id: UUID
     let url: String
+    
+    private enum CodingKeys: String, CodingKey { case id, url }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = UUID()
+        let decodedURL = try container.decode(String.self, forKey: .url)
+        url = decodedURL
+    }
 }
