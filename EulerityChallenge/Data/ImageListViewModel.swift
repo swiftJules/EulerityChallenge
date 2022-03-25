5933adf3d1db33ad46d3e66f467bde391ebcf096
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-25 07:53:35 -0400

    refactor so images are fetched and stored to cache after data is fetched

--- EulerityChallenge/Data/ImageListViewModel.swift
+++ EulerityChallenge/Data/ImageListViewModel.swift
@@ -6,15 +6,23 @@
 //
 
 import Foundation
+import UIKit
 
-class ImageListViewModel: ObservableObject {
+class ImageListViewModel: ObservableObject, Identifiable {
     @Published var images =  [ImageModel]()
     
     init() {
         DataRequester.shared.fetchData() { imageModels in
             DispatchQueue.main.async {
                 self.images = imageModels
+                self.fetchImages()
             }
+        }
+    }
+    
+    func fetchImages() {
+        for model in images {
+            ImageRequester.shared.fetchImage(imageStr: model.url)
         }
     }
 }
