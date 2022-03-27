d6e2940598f72e75656992b7b6fefca1a4a63a68
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-27 09:00:44 -0400

    add binding variables and set image frame

--- EulerityChallenge/View/PencilKitView.swift
+++ EulerityChallenge/View/PencilKitView.swift
@@ -5,16 +5,36 @@
 //  Created by Rave Bizz on 3/26/22.
 //
 
+import PencilKit
 import SwiftUI
 
-struct PencilKitView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct PencilKitView: UIViewRepresentable {
+    @Binding var editor: PKCanvasView
+    @Binding var imageData: Data
+    @Binding var toolPicker: PKToolPicker
+    let screen = UIScreen.main.bounds
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        editor.drawingPolicy = .anyInput
+        
+        if let image = UIImage(data: imageData) {
+            let imageView = UIImageView(image: image)
+            imageView.frame = CGRect(x: 0, y: -150, width: screen.width, height: screen.height)
+            imageView.contentMode = .scaleAspectFit
+            
+            let subView = editor.subviews[0]
+            subView.addSubview(imageView)
+            subView.sendSubviewToBack(imageView)
+        }
+        
+        toolPicker.setVisible(true, forFirstResponder: editor)
+        toolPicker.addObserver(editor)
+        editor.becomeFirstResponder()
+        
+        return editor
     }
-}
-
-struct PencilKitView_Previews: PreviewProvider {
-    static var previews: some View {
-        PencilKitView()
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        
     }
 }
