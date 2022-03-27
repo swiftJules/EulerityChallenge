d4a4b70cdad8843be7fa959be0d8a1ef188411c3
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-27 08:59:17 -0400

    change placeholder opacity

--- EulerityChallenge/View/ImageList.swift
+++ EulerityChallenge/View/ImageList.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ImageList: View {
     @ObservedObject var viewModel = ImageListViewModel()
+    // @ObservedObject var loaded: Bool = false
     
     var body: some View {
         NavigationView {
@@ -16,6 +17,8 @@ struct ImageList: View {
                 if let viewModel = viewModel {
                     ForEach(viewModel.detailViewModels) { detailViewModel in
                         NavigationLink(destination: ImageDetail(viewModel: detailViewModel)) {
+                            //ImageView(viewModel: detailViewModel)
+                            
                             AsyncImage(url: URL(string: detailViewModel.url)!) { image in
                                 image
                                     .resizable()
@@ -23,6 +26,7 @@ struct ImageList: View {
                             } placeholder: {
                                 Image(systemName: "photo.circle.fill")
                                     .resizable()
+                                    .opacity(0.2)
                                     .scaledToFit()
                             }
                         }
