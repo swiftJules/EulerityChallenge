0ecd41be9d12fd739215359a1bbff9f5c21a5666
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-27 08:56:37 -0400

    remove coding keys

--- EulerityChallenge/Data/ImageModel.swift
+++ EulerityChallenge/Data/ImageModel.swift
@@ -7,16 +7,6 @@
 
 import Foundation
 
-struct ImageModel: Codable, Identifiable {
-    var id: UUID
+struct ImageModel: Codable {
     let url: String
-    
-    private enum CodingKeys: String, CodingKey { case id, url }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = UUID()
-        let decodedURL = try container.decode(String.self, forKey: .url)
-        url = decodedURL
-    }
 }
