58a49bdf7c6e0c4b63b53e6f5d4f1c02bdb281bd
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-27 08:58:35 -0400

    add drawing view model environment object

--- EulerityChallenge/View/ImageEditor.swift
+++ EulerityChallenge/View/ImageEditor.swift
@@ -8,33 +8,13 @@
 import PencilKit
 import SwiftUI
 
-struct ImageEditor: UIViewRepresentable {
-    var editor = PKCanvasView()
-    let toolkit = PKToolPicker.init()
-    var image: UIImage?
+struct ImageEditor: View {
+    @EnvironmentObject var model: DrawingViewModel
+    var viewModel: ImageDetailViewModel?
     
-    func makeUIView(context: Context) -> PKCanvasView {
-        //self.editor.tool = PKInkingTool(.pen, color: .label)
-        self.editor.becomeFirstResponder()
-        return editor
-    }
-    
-    func updateUIView(_ uiView: PKCanvasView, context: Context) {
-        if let image = image {
-            let imageView = UIImageView(image: image)
-            imageView.frame = CGRect(x: 50, y: 50, width: 200, height: 200)
-            imageView.contentMode = .scaleToFill
-            imageView.clipsToBounds = true
-            
-            let subView = editor.subviews[0]
-            subView.addSubview(imageView)
-            subView.sendSubviewToBack(imageView)
-        }
-        
-        toolkit.addObserver(editor)
-        toolkit.setVisible(true, forFirstResponder: uiView)
-        DispatchQueue.main.async {
-            uiView.becomeFirstResponder()
+    var body: some View {
+        ZStack {
+            PencilKitView(editor: $model.editor, imageData: $model.imageData, toolPicker: $model.toolPicker)
         }
     }
 }
