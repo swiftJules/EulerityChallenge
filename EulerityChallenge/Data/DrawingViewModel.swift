a643127d74e69bb5e6178a988c8160b3b33e0fff
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-27 08:52:54 -0400

    add header

---
+++ EulerityChallenge/Data/DrawingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DrawingViewModel.swift
+//  EulerityChallenge
+//
+//  Created by Rave Bizz on 3/26/22.
+//
+
+import Foundation
---
+++ EulerityChallenge/View/PencilKitView.swift
@@ -0,0 +1,20 @@
+//
+//  PencilKitView.swift
+//  EulerityChallenge
+//
+//  Created by Rave Bizz on 3/26/22.
+//
+
+import SwiftUI
+
+struct PencilKitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PencilKitView_Previews: PreviewProvider {
+    static var previews: some View {
+        PencilKitView()
+    }
+}
