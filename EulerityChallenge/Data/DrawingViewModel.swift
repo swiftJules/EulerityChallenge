0eee3a6825b9b12409ea3fa1d0529d7071cdc93b
Juliana Connors <juliana.connors.dev@gmail.com>
2022-03-27 08:53:31 -0400

    add drawing view model

--- EulerityChallenge/Data/DrawingViewModel.swift
+++ EulerityChallenge/Data/DrawingViewModel.swift
@@ -5,4 +5,15 @@
 //  Created by Rave Bizz on 3/26/22.
 //
 
-import Foundation
+import PencilKit
+import SwiftUI
+
+class DrawingViewModel: ObservableObject {
+    @Published var imageData: Data = Data(count: 0)
+    @Published var editor = PKCanvasView()
+    @Published var toolPicker = PKToolPicker()
+    
+    func cancelEditing() {
+        imageData = Data(count: 0)
+    }
+}
